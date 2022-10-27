b69180446c4de3cc3940eb1cbb48d8e185c24c3c
hirschmi <hirschmi@hu-berlin.de>
2022-10-27 23:03:46 +0200

    new benchmarks

--- benchmark/visualize.py
+++ benchmark/visualize.py
@@ -66,6 +66,49 @@ def boxplots(dfs: List[pd.DataFrame], benchmark_name: str, save_boxplots: bool, 
     plt.show()
     plt.clf()
 
+def times(dfs: List[pd.DataFrame], benchmark_name: str, save_boxplots: bool, base_column: str):
+    get_intervals_time_dict = {}
+    transform_time_dict = {}
+    fit_randomforest_time_dict = {}
+
+    for i, result_df in enumerate(dfs):
+        boxplot_name = result_df[base_column][0] # hier immer den Parameter wählen der variiert wird
+        get_intervals_time_dict[boxplot_name] = result_df['get_intervals_time']
+        transform_time_dict[boxplot_name] = result_df['transform_time']
+        fit_randomforest_time_dict[boxplot_name] = result_df['fit_randomforest_time']
+
+    get_intervals_time_dfs = pd.DataFrame(get_intervals_time_dict)
+    transform_time_dfs = pd.DataFrame(transform_time_dict)
+    fit_randomforest_time_dfs = pd.DataFrame(fit_randomforest_time_dict)
+
+
+    sns.set_style('white')
+    sns.set(font_scale=1.8)
+
+    sns.barplot(data=get_intervals_time_dfs, estimator=np.mean, capsize=.2).set_title('get_intervals')
+    plt.xticks(rotation=90)
+    plt.xlabel(base_column) # hier immer den Parameter wählen der variiert wird
+    plt.ylabel("seconds")
+    if(save_boxplots): plt.savefig("./results/" + benchmark_name + "/" + benchmark_name + "_time_get_intervals.png", bbox_inches="tight")
+    plt.show()
+    plt.clf()
+
+    sns.barplot(data=transform_time_dfs, estimator=np.mean, capsize=.2).set_title('transform_time')
+    plt.xticks(rotation=90)
+    plt.xlabel(base_column) # hier immer den Parameter wählen der variiert wird
+    plt.ylabel("seconds")
+    if(save_boxplots): plt.savefig("./results/" + benchmark_name + "/" + benchmark_name + "_time_transform.png", bbox_inches="tight")
+    plt.show()
+    plt.clf()
+
+    sns.barplot(data=fit_randomforest_time_dfs, estimator=np.mean, capsize=.2).set_title('fit_randomforest_time')
+    plt.xticks(rotation=90)
+    plt.xlabel(base_column) # hier immer den Parameter wählen der variiert wird
+    plt.ylabel("seconds")
+    if(save_boxplots): plt.savefig("./results/" + benchmark_name + "/" + benchmark_name + "_time_fit_randomforest.png", bbox_inches="tight")
+    plt.show()
+    plt.clf()
+
 # for visualizing overall mean numbers from the _av csv files (currently not used)
 # def barplots(dfs: List[pd.DataFrame], benchmark_name: str, save_barcharts: bool, base_column: str):
 #     acc_dict = {}
